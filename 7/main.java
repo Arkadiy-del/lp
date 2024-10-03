43707e8f62d5a00c3879ea3e4880f1782ba2ded5
Arkadiy-del <82815983+Arkadiy-del@users.noreply.github.com>
2024-10-04 00:39:41 +0700

    Add files via upload

---
+++ 7/main.java
@@ -0,0 +1,18 @@
+//21. Дан прямоугольный треугольник с катетами a и b. С помощью лямбда-выражения найти радиус вписанной в треугольник окружности.
+
+import java.util.function.BiFunction;
+
+class main {
+    public static void main(String[] args) {
+
+        BiFunction<Double, Double, Double> hypotenuse = (a, b) -> Math.sqrt(a * a + b * b);
+
+        BiFunction<Double, Double, Double> radius = (a, b) -> (a + b - hypotenuse.apply(a, b)) / 2;
+
+        double a = 8.0;
+        double b = 6.0;
+        double r = radius.apply(a, b);
+
+        System.out.println("Радиус вписанной окружности: " + r);
+    }
+}
